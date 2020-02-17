93ead5129e2b4508cb45dd0c406f45384f97e3ae
jp-t-o-recruit <takahiro.oota.recruit@gmail.com>
2020-02-17 11:32:39 +0900

    3 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { environment } from '../environments/environment';
+import { UiModule } from './ui/ui.module';
+import { HelloComponent } from './hello.component.ts';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HelloComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    UiModule,
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+  declarations: [],
+  exports: []
+})
+export class UiModule { }
